1ffc92699f1e2f62581e05c41306d132eb966db9
Kangdy <canfrog@naver.com>
2022-04-06 23:25:54 +0900

    Make Searching Page

--- Node_PageBoard/routes/posts.js
+++ Node_PageBoard/routes/posts.js
@@ -6,27 +6,31 @@ let Post = require('../models/Post');
 let util = require('../util'); 
 
 // Index
-router.get('/', async function(req, res){ // 1
-    let page = Math.max(1, parseInt(req.query.page));   // 2
-    let limit = Math.max(1, parseInt(req.query.limit)); // 2
-    page = !isNaN(page)?page:1;                         // 3
-    limit = !isNaN(limit)?limit:10;                     // 3
+router.get('/', async function(req, res){ 
+    let page = Math.max(1, parseInt(req.query.page));   
+    let limit = Math.max(1, parseInt(req.query.limit)); 
+    page = !isNaN(page)?page:1;                         
+    limit = !isNaN(limit)?limit:10;                     
 
-    let skip = (page-1)*limit; // 4
-    let count = await Post.countDocuments({}); // 5
-    let maxPage = Math.ceil(count/limit); // 6
-    let posts = await Post.find({}) // 7
+    let searchQuery = createSearchQuery(req.query); 
+
+    let skip = (page-1)*limit; 
+    let count = await Post.countDocuments(searchQuery);
+    let maxPage = Math.ceil(count/limit); 
+    let posts = await Post.find(searchQuery) 
         .populate('author')
         .sort('-createdAt')
-        .skip(skip)   // 8
-        .limit(limit) // 8
+        .skip(skip)  
+        .limit(limit)
         .exec();
     
     res.render('posts/index', {
         posts:posts,
-        currentPage:page, // 9
-        maxPage:maxPage,  // 9
-        limit:limit       // 9
+        currentPage:page, 
+        maxPage:maxPage,  
+        limit:limit,
+        searchType:req.query.searchType, 
+        searchText:req.query.searchText         
     });
 });
 
@@ -46,17 +50,33 @@ router.post('/', util.isLoggedin, function(req, res){
             req.flash('errors', util.parseError(err));
             return res.redirect('/posts/new'+res.locals.getPostQueryString());
         }
-        res.redirect('/posts'+res.locals.getPostQueryString(false, {page:1}));
+        res.redirect('/posts'+res.locals.getPostQueryString(false, {page:1, searchText:''}));
     });
 });
 
+function createSearchQuery(queries){ 
+    let searchQuery = {};
+    if(queries.searchType && queries.searchText && queries.searchText.length >= 2){
+        let searchTypes = queries.searchType.toLowerCase().split(',');
+        let postQueries = [];
+        if(searchTypes.indexOf('title')>=0){
+            postQueries.push({ title: { $regex: new RegExp(queries.searchText, 'i') } });
+        }
+        if(searchTypes.indexOf('body')>=0){
+            postQueries.push({ body: { $regex: new RegExp(queries.searchText, 'i') } });
+        }
+        if(postQueries.length > 0) searchQuery = {$or:postQueries};
+    }
+    return searchQuery;
+}
+
 // show
 router.get('/:id', function(req, res){
-    Post.findOne({_id:req.params.id}) // 3
-      .populate('author')             // 3
-      .exec(function(err, post){      // 3
-        if(err) return res.json(err);
-        res.render('posts/show', {post:post});
+    Post.findOne({_id:req.params.id}) 
+        .populate('author')             
+        .exec(function(err, post){      
+            if(err) return res.json(err);
+            res.render('posts/show', {post:post});
     });
 });
 
