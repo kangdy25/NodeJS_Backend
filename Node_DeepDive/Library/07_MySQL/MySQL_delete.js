702d981f4ea7f084d0ad4c67798f89d7f7fef1d5
Kangdy <canfrog@naver.com>
2023-05-04 12:00:20 +0900

    Learn SQL

--- Node_DeepDive/Library/07_MySQL/MySQL_delete.js
+++ Node_DeepDive/Library/07_MySQL/MySQL_delete.js
@@ -1,6 +1,7 @@
-// mysql 모듈
+// mysql 모듈 사용
 const mysql = require('mysql');
 
+// 연결할 DB 정보입력
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -9,16 +10,20 @@ const connection = mysql.createConnection({
     port: '3306',
 });
 
+// 데이터베이스 연결
 connection.connect();
 
-connection.query('create table books (number INT NOT NULL AUTO_INCREMENT PRIMARY KEY, genre VARCHAR(20) NOT NULL, name VARCHAR(50) NOT NULL, writer VARCHAR(30) NOT NULL, releasedate date NOT NULL);', (error, results, fields)=>{
+// Delete 쿼리문 사용
+connection.query('delete from books where number = 2 and writer = \'JI\';', (error, results, fields) => {
     if (error) throw error;
     console.log(results);
 });
 
-connection.query('describe books', (error, results, fields) => {
+// Select 쿼리문 사용
+connection.query('SELECT * from books', (error, results, fields) => {
     if (error) throw error;
     console.log(results);
-})
+});
 
+// 연결 종료
 connection.end();
