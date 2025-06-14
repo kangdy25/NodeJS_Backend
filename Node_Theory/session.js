6cf4c954a0748e00854dc422e5e80d37b232603b
Kangdy <canfrog@naver.com>
2025-06-14 20:54:48 +0900

    Learn How to use REPL

---
+++ Node_Theory/session.js
@@ -0,0 +1,17 @@
+let x = 10
+x
+console.log(x);
+x
+_ + _
+for (let i = 0; i < 5; i++) {
+console.log(i);
+}
+for (let i = 1; i <= 3; i++) {
+    console.log(i * 2);
+}
+
+function sayBye(name) {
+    return `Bye, ${name}!`;
+}
+
+console.log(sayBye("Bob"));
