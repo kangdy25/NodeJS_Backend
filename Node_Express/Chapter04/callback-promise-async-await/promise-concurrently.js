bb66e26fa896524a549bdf7ef1da3870f6768e6b
Shineast <canfrog@naver.com>
2025-09-08 23:48:54 +0900

    Run multiple Promises concurrently

---
+++ Node_Express/Chapter04/callback-promise-async-await/promise-concurrently.js
@@ -0,0 +1,42 @@
+const DB = [];
+
+function saveDB(user) {
+  const oldDBSize = DB.length;
+  DB.push(user);
+  console.log(`save ${user.name} to DB`);
+  return new Promise((resolve, reject) => {
+    if (DB.length > oldDBSize) {
+      resolve(user);
+    } else {
+      reject(new Error("Save DB Error!"));
+    }
+  });
+}
+
+function sendEmail(user) {
+  console.log(`email to ${user.email}`);
+  return new Promise((resolve) => {
+    resolve(user);
+  });
+}
+
+function getResult(user) {
+  return new Promise((resolve, reject) => {
+    resolve(`success register ${user.name}`);
+  });
+}
+
+function registerByPromise(user) {
+  const result = saveDB(user).then(sendEmail).then(getResult);
+  console.log(result);
+  return result;
+}
+
+const myUser = {
+  email: "canfrog@naver.com",
+  password: "Idon'tknow",
+  name: "annonymous",
+};
+
+allResult = Promise.all([saveDB(myUser), sendEmail(myUser), getResult(myUser)]);
+allResult.then(console.log);
