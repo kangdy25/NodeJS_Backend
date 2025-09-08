27ff2bf3b0ab9a87be1cee7a189d009cb572b033
Shineast <canfrog@naver.com>
2025-09-08 23:44:24 +0900

    Learn Promise

---
+++ Node_Express/Chapter04/callback-promise-async-await/promise-test.js
@@ -0,0 +1,41 @@
+const DB = [];
+
+function saveDB(user) {
+  const oldDBSize = DB.length;
+  DB.push(user);
+  console.log(`save ${user.name} to DB`);
+  return new Promise((resolve, reject) => {
+    if (DB.length > oldDBSize) {
+      resolve(user);
+    } else {
+      reject(new Error("Save DB Error!"));
+    }
+  });
+}
+
+function sendEmail(user) {
+  console.log(`email to ${user.email}`);
+  return new Promise((resolve) => {
+    resolve(user);
+  });
+}
+
+function getResult(user) {
+  return new Promise((resolve, reject) => {
+    resolve(`success register ${user.name}`);
+  });
+}
+
+function registerByPromise(user) {
+  const result = saveDB(user).then(sendEmail).then(getResult);
+  console.log(result);
+  return result;
+}
+
+const myUser = {
+  email: "canfrog@naver.com",
+  password: "Idon'tknow",
+  name: "annonymous",
+};
+const result = registerByPromise(myUser);
+result.then(console.log);
