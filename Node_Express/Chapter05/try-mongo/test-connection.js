4b42bb55c01abc2ec90af67e1b9c73b1c4c1beef
Shineast <canfrog@naver.com>
2025-09-11 00:26:17 +0900

    MongoDB Atlas Setup

---
+++ Node_Express/Chapter05/try-mongo/test-connection.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_PASSWORD;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  await client.connect();
+  const adminDB = client.db("test").admin();
+  const listDatabases = await adminDB.listDatabases();
+  console.log(listDatabases);
+  return "OK";
+}
+
+run()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
