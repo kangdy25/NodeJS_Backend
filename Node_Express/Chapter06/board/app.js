7525a8523687baed9d4f3ac37e89893109f2df81
Shineast <canfrog@naver.com>
2025-09-12 16:15:53 +0900

    Write UI

--- Node_Express/Chapter06/board/app.js
+++ Node_Express/Chapter06/board/app.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
   res.render("home", { title: "테스트 게시판" });
 });
 
+app.get("/write", (req, res) => {
+  res.render("write", { title: "테스트 게시판" });
+});
+
 app.listen(3000, () => {
   console.log("SERVER START");
 });
