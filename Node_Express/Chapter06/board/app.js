30633899ae26da840fc3312e855cfc2b69503cb2
Kangdy <canfrog@naver.com>
2025-09-13 23:17:54 +0900

    Utility for mongodb connection

--- Node_Express/Chapter06/board/app.js
+++ Node_Express/Chapter06/board/app.js
@@ -1,23 +1,32 @@
+require("dotenv").config();
 const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
+
+const mongodbConnection = require("./configs/mongodb-connection");
 
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-  res.render("home", { title: "테스트 게시판" });
+    res.render("home", { title: "테스트 게시판" });
 });
 
 app.get("/write", (req, res) => {
-  res.render("write", { title: "테스트 게시판" });
+    res.render("write", { title: "테스트 게시판" });
 });
 
 app.get("/detail/:id", async (req, res) => {
-  res.render("detail", { title: "테스트 게시판" });
+    res.render("detail", { title: "테스트 게시판" });
 });
 
-app.listen(3000, () => {
-  console.log("SERVER START");
+let collection;
+
+app.listen(3000, async () => {
+    console.log("SERVER START");
+
+    const mongoClient = await mongodbConnection();
+    collection = mongoClient.db().collection("post");
+    console.log("MONGODB CONNECTED");
 });
---
+++ Node_Express/Chapter06/board/configs/mongodb-connection.js
@@ -0,0 +1,7 @@
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+
+module.exports = function (callback) {
+    return MongoClient.connect(uri, callback);
+};
