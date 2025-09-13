4e2849165e1e40e6623445d4af8c2eaf4b910ede
Kangdy <canfrog@naver.com>
2025-09-14 00:03:31 +0900

    Write API

--- Node_Express/Chapter06/board/app.js
+++ Node_Express/Chapter06/board/app.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const handlebars = require("express-handlebars");
+const mongodbConnection = require("./configs/mongodb-connection");
+const postService = require("./services/post-service");
+
 const app = express();
 
-const mongodbConnection = require("./configs/mongodb-connection");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.engine(
     "handlebars",
@@ -19,8 +23,17 @@ app.get("/", (req, res) => {
     res.render("home", { title: "테스트 게시판" });
 });
 
+// 쓰기 페이지 이동
 app.get("/write", (req, res) => {
     res.render("write", { title: "테스트 게시판" });
+});
+
+// 글쓰기
+app.post("/write", async (req, res) => {
+    const post = req.body;
+    const result = await postService.writePost(collection, post);
+    console.log(result);
+    res.redirect(`/detail/${result.insertedId}`);
 });
 
 app.get("/detail/:id", async (req, res) => {
