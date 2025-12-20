6f13078a7f266369fbc2d4d1511566e63b771d3d
Shineast <canfrog@naver.com>
2025-12-20 18:54:21 +0900

    Creating an API that stores data in memory

--- Node_NestJS/nest-js_blog/src/blog.controller.ts
+++ Node_NestJS/nest-js_blog/src/blog.controller.ts
@@ -7,35 +7,49 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { BlogService } from './blog.service';
 
 @Controller('blog')
 export class BlogController {
-  // Get 요청 처리
+  blogService: BlogService;
+  constructor() {
+    this.blogService = new BlogService();
+  }
+
+  // GET 요청 처리
   @Get()
   getAllPosts() {
     console.log('모든 게시글 가져오기');
+    return this.blogService.getAllPosts();
   }
 
-  // Post 요청 처리
+  // POST 요청 처리
   @Post()
-  createPost(@Body() post: any) {
+  createPost(@Body() postDto) {
     console.log('게시글 작성');
-    console.log(post);
+    this.blogService.createPost(postDto);
+    return 'success';
   }
 
+  // GET에 URL 매개변수에 id가 있는 요청 처리
   @Get('/:id')
   getPost(@Param('id') id: string) {
     console.log(`[id: ${id}게시글 하나 가져오기]`);
+    return this.blogService.getPost(id);
   }
 
+  // DELETE 방식에 URL 매개변수로 id가 있는 요청 처리
   @Delete('/:id')
-  deletePost() {
+  deletePost(@Param('id') id: string) {
     console.log('게시글 삭제');
+    this.blogService.delete(id);
+    return 'success';
   }
 
+  // PUT 방식에 URL 매개변수로 전달된 id가 있는 요청 처리
   @Put('/:id')
-  updatePost(@Param('id') id, @Body() post: any) {
-    console.log(`[${id} 게시글 업데이트]`);
-    console.log(post);
+  updatePost(@Param('id') id: string, @Body() postDto) {
+    console.log(`[${id} 게시글 업데이트]`, postDto);
+    return this.blogService.updatePost(id, postDto);
   }
 }
--- Node_NestJS/nest-js_blog/src/blog.service.ts
+++ Node_NestJS/nest-js_blog/src/blog.service.ts
@@ -1,1 +1,34 @@
-export class BlogService {}
+import { PostDto } from './blog.model';
+
+export class BlogService {
+  posts: any = [];
+
+  getAllPosts() {
+    return this.posts;
+  }
+
+  createPost(postDto: PostDto) {
+    const id = this.posts.length + 1;
+    this.posts.push({ ...postDto, id: id.toString(), createDt: new Date() });
+  }
+
+  getPost(id) {
+    const post = this.posts.find((post) => {
+      return post.id === id;
+    });
+    console.log(post);
+    return post;
+  }
+
+  delete(id) {
+    const filteredPosts = this.posts.filter((post) => post.id !== id);
+    this.posts = [...filteredPosts];
+  }
+
+  updatePost(id, postDto: PostDto) {
+    let updateIndex = this.posts.findIndex((post) => post.id === id);
+    const updatePost = { ...postDto, id, updateDt: new Date() };
+    this.posts[updateIndex] = updatePost;
+    return updatePost;
+  }
+}
