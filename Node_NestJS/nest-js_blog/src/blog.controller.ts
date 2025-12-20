7c142717cb393416d643b093b754877c7ab50ef9
Shineast <canfrog@naver.com>
2025-12-20 14:57:27 +0900

    Create Blog API

--- Node_NestJS/nest-js_blog/src/blog.controller.ts
+++ Node_NestJS/nest-js_blog/src/blog.controller.ts
@@ -1,1 +1,41 @@
-export class BlogController {}
+import {
+  Controller,
+  Param,
+  Body,
+  Delete,
+  Get,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+@Controller('blog')
+export class BlogController {
+  // Get 요청 처리
+  @Get()
+  getAllPosts() {
+    console.log('모든 게시글 가져오기');
+  }
+
+  // Post 요청 처리
+  @Post()
+  createPost(@Body() post: any) {
+    console.log('게시글 작성');
+    console.log(post);
+  }
+
+  @Get('/:id')
+  getPost(@Param('id') id: string) {
+    console.log(`[id: ${id}게시글 하나 가져오기]`);
+  }
+
+  @Delete('/:id')
+  deletePost() {
+    console.log('게시글 삭제');
+  }
+
+  @Put('/:id')
+  updatePost(@Param('id') id, @Body() post: any) {
+    console.log(`[${id} 게시글 업데이트]`);
+    console.log(post);
+  }
+}
